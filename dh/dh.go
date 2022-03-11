afaa2e5412396c780adcebd386a3258046b6455d
Oringik <zikalol2@mail.ru>
2022-03-11 18:42:23 +0300

    init commit

---
+++ dh/dh.go
@@ -0,0 +1,59 @@
+package dh
+
+import (
+	"crypto/rand"
+	"errors"
+	"math/big"
+)
+
+type KeyStore struct {
+	PrivateKey *big.Int
+	PublicKey  *big.Int
+	SharedKey  *big.Int
+}
+
+func (k *KeyStore) GenerateSharedKey(receivedPublicKey *big.Int) error {
+	sharedKey := new(big.Int)
+
+	if !IsKeyValid(receivedPublicKey) {
+		return errors.New("incorrect received public key")
+	}
+	if !IsKeyValid(k.PrivateKey) {
+		return errors.New("incorrect private key")
+	}
+
+	sharedKey.Exp(receivedPublicKey, k.PrivateKey, Prime)
+
+	k.SharedKey = sharedKey
+
+	return nil
+}
+
+func (k *KeyStore) GeneratePublicKey(privateNum *big.Int) {
+	publicKey := new(big.Int)
+	publicKey.Exp(Generator, privateNum, Prime)
+
+	k.PublicKey = publicKey
+}
+
+func (k *KeyStore) GeneratePrivateKey() {
+	privateKey, _ := rand.Int(rand.Reader, Prime)
+
+	k.PrivateKey = privateKey
+}
+
+func IsValidParams(g *big.Int, p *big.Int) error {
+	if g.Cmp(Generator) != 0 {
+		return errors.New("incorrect generator")
+	}
+
+	if p.Cmp(Prime) != 0 {
+		return errors.New("incorrect prime")
+	}
+
+	return nil
+}
+
+func IsKeyValid(key *big.Int) bool {
+	return key.BitLen() != 0
+}
