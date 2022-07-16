dc26bef1a1c2951573f80e5e7f8f8cd813baef83
oringik <zikalol2@mail.ru>
2022-07-16 22:09:02 +0300

    deleted extra handlers

--- generated/endpoint.go
+++ generated/endpoint.go
@@ -5,8 +5,6 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"github.com/Oringik/crypto-chateau/message"
-	"strconv"
 )
 
 type Endpoint struct {
@@ -20,10 +18,7 @@ type StreamI interface {
 
 type UserEndpoint interface {
 	SendCode(context.Context, *SendCodeRequest) (*SendCodeResponse, error)
-	GetUser(context.Context, *GetUserRequest) (*GetUserResponse, error)
-	CreateUser(context.Context, *CreateUserRequest) (*CreateUserResponse, error)
-	GetUsers(context.Context, *GetUsersRequest) (*GetUsersResponse, error)
-	GetUserUpdates(context.Context, StreamI) error
+	GetEvents(context.Context, StreamI) error
 }
 
 type SendCodeRequest struct {
@@ -34,36 +29,12 @@ type SendCodeRequest struct {
 type SendCodeResponse struct {
 }
 
-type GetUserRequest struct {
-	UserID uint64
-}
-
 type User struct {
 	Id       uint64
 	Nickname string
 	Age      int
 	Gender   bool
 	Status   string
-}
-
-type GetUserResponse struct {
-	User User
-}
-
-type GetUsersRequest struct {
-	Offset int
-	Limit  int
-}
-
-type GetUsersResponse struct {
-	Users []*User
-}
-
-type CreateUserRequest struct {
-	User *User
-}
-
-type CreateUserResponse struct {
 }
 
 func (i *SendCodeRequest) Marshal() []byte {
@@ -98,42 +69,6 @@ func (i *User) Marshal() []byte {
 	return marshalBytes
 }
 
-func (i *GetUserRequest) Marshal() []byte {
-	return []byte("GetUser# UserID:" + strconv.Itoa(int(i.UserID)))
-}
-
-func (i *GetUserResponse) Marshal() []byte {
-	strBytes := []byte("GetUser# User:")
-	strBytes = append(strBytes, i.User.Marshal()...)
-	return strBytes
-}
-
-func (i *GetUsersRequest) Marshal() []byte {
-	marshalStr := fmt.Sprintf("GetUsersRequest# Offset: %d, Limit: %d", i.Offset, i.Limit)
-	return []byte(marshalStr)
-}
-
-func (i *GetUsersResponse) Marshal() []byte {
-	var usersParam []byte
-	for j := 0; j < len(i.Users); j++ {
-		usersParam = append(usersParam, i.Users[j].Marshal()...)
-		if j < len(i.Users)-1 {
-			usersParam = append(usersParam, byte(','))
-		}
-	}
-	marshalStr := fmt.Sprintf("GetUsersResponse# Users: {%s}", string(usersParam))
-	return []byte(marshalStr)
-}
-
-func (i *CreateUserRequest) Marshal() []byte {
-	marshalStr := fmt.Sprintf("GetUser# User: %s", string(i.User.Marshal()))
-	return []byte(marshalStr)
-}
-
-func (i *CreateUserResponse) Marshal() []byte {
-	return nil
-}
-
 // unmarshal
 
 func (i *SendCodeRequest) Unmarshal(params map[string][]byte) error {
@@ -161,58 +96,5 @@ func (i *User) Unmarshal(params map[string][]byte) error {
 	}
 	i.Status = string(params["Status"])
 
-	return nil
-}
-
-func (i *GetUserRequest) Unmarshal(params map[string][]byte) error {
-	i.UserID = binary.BigEndian.Uint64(params["UserID"])
-
-	return nil
-}
-
-func (i *GetUserResponse) Unmarshal(params map[string][]byte) error {
-	return i.User.Unmarshal(params)
-}
-
-func (i *GetUsersRequest) Unmarshal(params map[string][]byte) error {
-	i.Offset = int(binary.BigEndian.Uint64(params["Offset"]))
-	i.Limit = int(binary.BigEndian.Uint64(params["Limit"]))
-
-	return nil
-}
-
-func (i *GetUsersResponse) Unmarshal(params map[string][]byte) error {
-	rawUsers, err := message.ParseArray(params["Users"])
-	if err != nil {
-		return err
-	}
-
-	users := make([]*User, 0, len(rawUsers))
-
-	for _, rawUser := range rawUsers {
-		userParams, err := message.GetParams(rawUser)
-		if err != nil {
-			return err
-		}
-
-		user := &User{}
-		err = user.Unmarshal(userParams)
-		if err != nil {
-			return err
-		}
-
-		users = append(users, user)
-	}
-
-	i.Users = users
-
-	return nil
-}
-
-func (i *CreateUserRequest) Unmarshal(params map[string][]byte) error {
-	return i.User.Unmarshal(params)
-}
-
-func (i *CreateUserResponse) Unmarshal(params map[string][]byte) error {
 	return nil
 }
--- handler.go
+++ handler.go
@@ -19,45 +19,19 @@ type Handler struct {
 }
 
 func initHandlers(endpoint generated.Endpoint, handlers map[string]*Handler) {
-	handlers["GetUser"] = &Handler{
-		callFunc:       endpoint.UserEndpoint.GetUser,
-		HandlerType:    HandlerT,
-		requestMsgType: &generated.GetUserRequest{},
-	}
 	handlers["SendCode"] = &Handler{
 		callFunc:       endpoint.UserEndpoint.SendCode,
 		HandlerType:    HandlerT,
 		requestMsgType: &generated.SendCodeRequest{},
 	}
-	handlers["GetUserUpdates"] = &Handler{
-		callFunc:    endpoint.UserEndpoint.GetUserUpdates,
+	handlers["GetEvents"] = &Handler{
+		callFunc:    endpoint.UserEndpoint.GetEvents,
 		HandlerType: StreamT,
-	}
-	handlers["CreateUser"] = &Handler{
-		callFunc:    endpoint.UserEndpoint.CreateUser,
-		HandlerType: HandlerT,
-	}
-	handlers["GetUsers"] = &Handler{
-		callFunc:    endpoint.UserEndpoint.GetUsers,
-		HandlerType: HandlerT,
 	}
 }
 
 func callFuncToHandlerFunc(fnc interface{}) (func(context.Context, message.Message) (message.Message, error), error) {
 	switch fnc.(type) {
-	case func(context.Context, *generated.GetUserRequest) (*generated.GetUserResponse, error):
-		callFunc := func(ctx context.Context, message message.Message) (message.Message, error) {
-			convertedMessage, ok := message.(*generated.GetUserRequest)
-			if !ok {
-				err := errors.New("error converting message to GetUserRequest")
-				return nil, err
-			}
-
-			resp, err := fnc.(func(context.Context, *generated.GetUserRequest) (*generated.GetUserResponse, error))(ctx, convertedMessage)
-			return resp, err
-		}
-
-		return callFunc, nil
 	case func(ctx context.Context, request *generated.SendCodeRequest) (*generated.SendCodeResponse, error):
 		callFunc := func(ctx context.Context, message message.Message) (message.Message, error) {
 			convertedMessage, ok := message.(*generated.SendCodeRequest)
@@ -67,32 +41,6 @@ func callFuncToHandlerFunc(fnc interface{}) (func(context.Context, message.Messa
 			}
 
 			resp, err := fnc.(func(context.Context, *generated.SendCodeRequest) (*generated.SendCodeResponse, error))(ctx, convertedMessage)
-			return resp, err
-		}
-
-		return callFunc, nil
-	case func(ctx context.Context, request *generated.GetUsersRequest) (*generated.GetUsersResponse, error):
-		callFunc := func(ctx context.Context, message message.Message) (message.Message, error) {
-			convertedMessage, ok := message.(*generated.GetUsersRequest)
-			if !ok {
-				err := errors.New("error converting message to GetUsersRequest")
-				return nil, err
-			}
-
-			resp, err := fnc.(func(context.Context, *generated.GetUsersRequest) (*generated.GetUsersResponse, error))(ctx, convertedMessage)
-			return resp, err
-		}
-
-		return callFunc, nil
-	case func(ctx context.Context, request *generated.CreateUserRequest) (*generated.CreateUserResponse, error):
-		callFunc := func(ctx context.Context, message message.Message) (message.Message, error) {
-			convertedMessage, ok := message.(*generated.CreateUserRequest)
-			if !ok {
-				err := errors.New("error converting message to GetUsersRequest")
-				return nil, err
-			}
-
-			resp, err := fnc.(func(context.Context, *generated.CreateUserRequest) (*generated.CreateUserResponse, error))(ctx, convertedMessage)
 			return resp, err
 		}
 
