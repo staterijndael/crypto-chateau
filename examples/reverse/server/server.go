b03b56c24e0788e8c17d50493d0382d72b594002
maxim-konovalov <maksim.konovalov@vk.team>
2022-11-13 20:11:16 +0300

    add logs for example server

--- examples/reverse/server/server.go
+++ examples/reverse/server/server.go
@@ -5,10 +5,13 @@ import (
 	endpoints "github.com/oringik/crypto-chateau/examples/reverse/codegen"
 	server2 "github.com/oringik/crypto-chateau/server"
 	zap2 "go.uber.org/zap"
+	"log"
 	"time"
 )
 
 func main() {
+	log.Println("server started")
+
 	zapInstance, _ := zap2.NewProduction()
 	reverseEndpoint := &ReverseEndpoint{}
 
@@ -31,6 +34,7 @@ func main() {
 type ReverseEndpoint struct{}
 
 func (r *ReverseEndpoint) ReverseMagicString(ctx context.Context, req *endpoints.ReverseMagicStringRequest) (*endpoints.ReverseMagicStringResponse, error) {
+	log.Printf("resived %s", req.MagicString)
 	magicStringRunes := []rune(req.MagicString)
 	left := 0
 	right := len(magicStringRunes) - 1
